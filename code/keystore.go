b14d0485e721f1581aa3fc4cafa929fa3b9301d7
Cesar Giron <72999160+afortunado21@users.noreply.github.com>
2021-03-18 15:51:42 -0500

    Update keystore.go

--- code/keystore.go
+++ code/keystore.go
@@ -17,11 +17,11 @@ func createKs() {
 		log.Fatal(err)
 	}
 
-	fmt.Println(account.Address.Hex()) // 0x20F8D42FB0F667F2E53930fed426f225752453b3
+	fmt.Println(account.Address.Hex()) // 0x2D170ce1F719476FeC1a92856cf632aE93444b41
 }
 
 func importKs() {
-	file := "./tmp/UTC--2018-07-04T09-58-30.122808598Z--20f8d42fb0f667f2e53930fed426f225752453b3"
+	file := 0x2D170ce1F719476FeC1a92856cf632aE93444b41"
 	ks := keystore.NewKeyStore("./tmp", keystore.StandardScryptN, keystore.StandardScryptP)
 	jsonBytes, err := ioutil.ReadFile(file)
 	if err != nil {
