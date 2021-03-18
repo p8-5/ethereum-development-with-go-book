4239711a3f34189912ff1637c1b6e1f736aca543
Cesar Giron <72999160+afortunado21@users.noreply.github.com>
2021-03-18 16:06:55 -0500

    Update wallet_generate.go

--- code/wallet_generate.go
+++ code/wallet_generate.go
@@ -29,9 +29,9 @@ func main() {
 	fmt.Println(hexutil.Encode(publicKeyBytes)[4:]) // 9a7df67f79246283fdc93af76d4f8cdd62c4886e8cd870944e817dd0b97934fdd7719d0810951e03418205868a5c1b40b192451367f28e0088dd75e15de40c05
 
 	address := crypto.PubkeyToAddress(*publicKeyECDSA).Hex()
-	fmt.Println(address) // 0x96216849c49358B10257cb55b28eA603c874b05E
+	fmt.Println(address) // 0x2D170ce1F719476FeC1a92856cf632aE93444b41
 
 	hash := sha3.NewLegacyKeccak256()
 	hash.Write(publicKeyBytes[1:])
-	fmt.Println(hexutil.Encode(hash.Sum(nil)[12:])) // 0x96216849c49358b10257cb55b28ea603c874b05e
+	fmt.Println(hexutil.Encode(hash.Sum(nil)[12:])) // 0x2D170ce1F719476FeC1a92856cf632aE93444b41
 }
