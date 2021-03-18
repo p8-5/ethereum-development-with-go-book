62aee2a71d0e8bf0ffa8e136a16b638856ad885b
Cesar Giron <72999160+afortunado21@users.noreply.github.com>
2021-03-18 16:01:31 -0500

    Update signature_generate.go

--- code/signature_generate.go
+++ code/signature_generate.go
@@ -16,12 +16,12 @@ func main() {
 
 	data := []byte("hello")
 	hash := crypto.Keccak256Hash(data)
-	fmt.Println(hash.Hex()) // 0x1c8aff950685c2ed4bc3174f3472287b56d9517b9c948127319a09a7a36deac8
+	fmt.Println(hash.Hex()) // 0x5b579DEbCD8f1cE2d5BA30Db13E72234Cb3D8664
 
 	signature, err := crypto.Sign(hash.Bytes(), privateKey)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(hexutil.Encode(signature)) // 0x789a80053e4927d0a898db8e065e948f5cf086e32f9ccaa54c1908e22ac430c62621578113ddbb62d509bf6049b8fb544ab06d36f916685a2eb8e57ffadde02301
+	fmt.Println(hexutil.Encode(signature)) // 0x304Cd3750060E18c54eCa2716C6AC5f9c180ed73
 }
